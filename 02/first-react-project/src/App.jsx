f99ff67d93e9ee8ce6648072e34a4dc6ad204872
Davi Dolabella <davimdolabella03@gmail.com>
2024-09-09 17:46:13 -0300

    Add files via upload

---
+++ 02/first-react-project/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App (){
+  return(
+    <div className="container">
+      <h1>React</h1>
+      <p>A Biblioteca para interfaces de usuário web e nativas</p>
+      <button className="btn btn-outline-primary rounded me-3">Aprenda React</button>
+      <button className="btn btn-outline-danger rounded">Referência de Api</button>
+      <hr/>
+      <h2>Crie interfaces de usuário de componentes</h2>
+      <p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados componentes</p>
+      <hr />
+    </div>
+  )
+}
